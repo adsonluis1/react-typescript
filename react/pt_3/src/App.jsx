69420bbffbb29b3242fc0f7642805a9e236d94bc
adsonluis1 <129302344+adsonluis1@users.noreply.github.com>
2023-12-19 11:01:14 -0300

    .

--- react/pt_3/src/App.jsx
+++ react/pt_3/src/App.jsx
@@ -6,8 +6,8 @@ function App() {
   
   return (
     <>
-      {/* <UseEfect /> */}
-      <Timer />
+      {/* <UseEfect /> 
+      <Timer /> */}
     </>
   )
 }
--- react/pt_3/src/components/Timer.jsx
+++ react/pt_3/src/components/Timer.jsx
@@ -11,7 +11,7 @@ function Timer (){
 
         return ()=> clearInterval(interval)
 
-   },[])
+   },{})
     
 
     return (
