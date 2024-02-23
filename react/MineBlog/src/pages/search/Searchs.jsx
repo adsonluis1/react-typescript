2a778cb6df4fa8f6213321719f2af325d32359bf
adsonluis1 <129302344+adsonluis1@users.noreply.github.com>
2024-02-23 09:35:24 -0300

    search

--- react/MineBlog/src/pages/search/Searchs.jsx
+++ react/MineBlog/src/pages/search/Searchs.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useFetchDocuments } from '../../hooks/useFetchDocuments'
 import  useQuery  from '../../hooks/useQuery'
-import { useLocation } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
+import Posts from '../../components/Posts'
+import styles from './Search.module.css'
 
 const Searchs = () => {
-  const query = useQuery()
-  const url =query.get('q')
-    return (
-    <div>
-        <h1>{url}</h1>
-    </div>
+  const url = useQuery()
+  const query=url.get('q')
+  const {documents:posts, error} = useFetchDocuments('posts', query)
+  const navigate = useNavigate()
+  console.log(posts)
+  return (
+    <section className={styles.section}>
+        {error && <h2>{error}</h2>}
+        {posts && 
+        <Posts posts={posts} />}
+        {posts && posts.length === 0 &&
+        <div className={styles.filha}>
+          <h1>Ops, parace que não tem nenhum post com essa tag 🥺</h1>
+          <button className={styles.btn} onClick={()=>navigate('/posts/create')}>Criar seu post</button>
+        </div>}
+    </section>
   )
 }
 
