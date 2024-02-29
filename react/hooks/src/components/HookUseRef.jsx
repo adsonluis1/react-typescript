2a5b6855e2862a971bcc599d4502ecc182a12698
adsonluis1 <129302344+adsonluis1@users.noreply.github.com>
2024-02-29 10:32:19 -0300

    useRef com DOM

--- react/hooks/src/components/HookUseRef.jsx
+++ react/hooks/src/components/HookUseRef.jsx
@@ -5,8 +5,23 @@ const HookUseRef = () => {
     let countRederização = useRef(0)
     const  [count, setCount] = useState(0)
     const  [countB, setCountB] = useState(0)
-    
+    const [t, setT] = useState('')
+
+
     useEffect(()=>{countRederização.current= countRederização.current+1})
+    // toda vez que o useState renderiza a pagina por ser mudado de valor o useRef aumenta +1 do seu valor
+
+    // useRef com Dom
+    const inputRef = useRef()
+
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+        // pode ser usado igual o js native 
+        inputRef.current.setAttribute('class', 'minha-class')
+        inputRef.current.focus()
+
+        setT('')
+    }
 
     return (
     <div>
@@ -17,6 +32,20 @@ const HookUseRef = () => {
         <button onClick={()=> setCount((prev)=> prev + 1)}>click Count</button>
         <h2>count:{countB}</h2>
         <button onClick={()=> setCountB((prev)=> prev + 1)}>click Count B</button>
+        
+        <h2>UseRef com DOM</h2>
+
+        <form onClick={handleSubmit}>
+            <input 
+            type="text"
+            ref={inputRef}
+            value={t}
+            onChange={(e)=> setT(e.target.value)
+            }
+            />
+
+            <input type="submit" value="enviar" />
+        </form>
     </div>
   )
 }
