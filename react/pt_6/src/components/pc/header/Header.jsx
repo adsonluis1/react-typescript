c6d0afc1dd2842ae1ce778b6925908665f050d60
adsonluis1 <dev.adsonluis@gmail.com>
2024-04-08 12:07:21 -0300

    components pc

---
+++ react/pt_6/src/components/pc/header/Header.jsx
@@ -0,0 +1,21 @@
+// header pc
+import React from 'react'
+import style from './Header.module.css'
+const Header = () => {
+  return (
+    <header className={style.header}>
+        <nav>
+            <ul>
+                <li>ação 1</li>
+                <li>ação 2</li>
+                <li>ação 3</li>
+                <li>ação 4</li>
+                <li>ação 5</li>
+                <li>ação 6</li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
+
+export default Header
