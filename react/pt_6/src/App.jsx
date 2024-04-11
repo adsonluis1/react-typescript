b6435ae2946473eec39f50e3bd51756542438066
adsonluis1 <dev.adsonluis@gmail.com>
2024-04-11 11:22:03 -0300

    ErrorBoundary

--- react/pt_6/src/App.jsx
+++ react/pt_6/src/App.jsx
@@ -3,17 +3,26 @@ import { useState, useEffect } from 'react'
 import UseMediaQuery from './hooks/UseMediaQuery'
 import HomeM from './pages/mobile/Home/HomeM'
 import HomeP from './pages/pc/Home/HomeP'
+import Count from './components/Count'
+import ErrorBoundary from './components/ErrorBoudary'
 
 
 
 
 function App() {
-  const typeScreen = UseMediaQuery()
+  const typeScreen = false
+  // UseMediaQuery()
   
   return (
     <>
-    {typeScreen !='pc' && typeScreen !='notbook' && <HomeM />}
-    {typeScreen !='celular' && typeScreen !='tablet' && <HomeP />}
+      <h1>com ErrorBoundary</h1>
+      <ErrorBoundary>
+        <Count />
+      </ErrorBoundary>
+      <h1>sem ErrorBoundary</h1>
+      <Count />
+    {typeScreen && typeScreen !='pc' && typeScreen !='notbook' && <HomeM />}
+    {typeScreen && typeScreen !='celular' && typeScreen !='tablet' && <HomeP />}
     </>
   )
 }
