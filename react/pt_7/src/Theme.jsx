6db9f239de362a0c2e41eecfbf2e5d0ce18c5947
adsonluis1 <dev.adsonluis@gmail.com>
2024-05-08 12:01:27 -0300

    Theme

---
+++ react/pt_7/src/Theme.jsx
@@ -0,0 +1,11 @@
+const Theme = {
+    color:{
+        main:'red',
+        secondary:'black',
+        tertiary:'white',
+        houver:'#c7c7c7'
+    },
+    fontFamily:'Courier New, monospace',
+}
+
+export default Theme
