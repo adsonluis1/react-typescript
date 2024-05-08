24936385693d550a98192330e59da346f7dcc324
adsonluis1 <dev.adsonluis@gmail.com>
2024-05-08 11:58:50 -0300

    Nav-style

---
+++ react/pt_7/src/style/components/Nav.style.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Nav = styled.nav`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+    background-color: black;
+    height: 50px;
+    width: 100%;
+    margin-bottom: 10px;
+
+`
