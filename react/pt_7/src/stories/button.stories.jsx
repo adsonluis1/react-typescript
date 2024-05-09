4ac5ee693e87d0f7ae6e655b33470c1632254ad7
adsonluis1 <dev.adsonluis@gmail.com>
2024-05-09 12:16:11 -0300

    button.stories

---
+++ react/pt_7/src/stories/button.stories.jsx
@@ -0,0 +1,19 @@
+import Button from '../style/components/button.style';
+
+export default {
+    component:Button
+}
+
+export const FirstStory = {
+    args:{
+        $select:true,
+        label:'Home'
+    }
+}
+
+export const SecondStory = {
+    args:{
+        $select:false,
+        label:'Home'
+    }
+}
