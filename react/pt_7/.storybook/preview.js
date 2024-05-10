fea61631f8c2003e3c234391532c4cb2d40d6396
adsonluis1 <dev.adsonluis@gmail.com>
2024-05-10 12:05:21 -0300

    preview-storybook

--- react/pt_7/.storybook/preview.js
+++ react/pt_7/.storybook/preview.js
@@ -3,7 +3,19 @@ import { withThemeFromJSXProvider } from '@storybook/addon-themes';
 import Theme from '../src/Theme';
 
 /* TODO: update import for your custom theme configurations */
-
+export const parameters = {
+  parameters:{
+    backgrounds:{
+      default:'dark',
+      values:[
+        {name:'green',value:'#00ff1a'},
+        {name:'purple',value:'#800080'},
+        {name:'dark',value:'#333'},
+        {name:'white',value:'#fff'}
+      ],
+    },
+  },
+}
 
 /* TODO: replace with your own global styles, or remove */
 const GlobalStyles = createGlobalStyle`
