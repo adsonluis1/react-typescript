465fb46e28b23f47d087b786114e95c02edd2c36
adsonluis1 <dev.adsonluis@gmail.com>
2024-05-10 12:11:52 -0300

    stories Menu

---
+++ react/pt_7/src/stories/Menu.stories.jsx
@@ -0,0 +1,15 @@
+import Menu from "../components/Menu";
+
+export default {
+    component:Menu
+}
+
+export const FirstStorys = {
+    argTypes: {
+        backgroundColor: { control: 'color' }
+      },
+    
+    args:{
+        backgroundColor:'#ff0303'
+    }
+}
