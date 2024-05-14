8bc2549e8b66fa929aa4639a0a1d90c44179d071
adsonluis1 <dev.adsonluis@gmail.com>
2024-05-14 11:49:29 -0300

    Ajustando Menu

--- react/pt_7/src/components/Menu.jsx
+++ react/pt_7/src/components/Menu.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react'
 import { Nav } from '../style/components/Nav.style'
 import Button from '../style/components/button.style'
+import Theme from '../Theme'
 
-const Menu = ({backgroudColor}) => {
+const Menu = () => {
     const [select, setSelect] = useState('')
    
   return (
-    <Nav>
+    <Nav $backgroundColor={(Theme.color.secondary)}>
         {select== 'home' && <Button $select={true} onClick={()=> setSelect('')}>Home</Button>}
         {select!= 'home' && <Button $select={false} onClick={()=> setSelect('home')}>Home</Button>}
         {select == 'assinaturas' && <Button $select={true} onClick={()=> setSelect('')}>Assinaturas</Button>}
