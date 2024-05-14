807e403e3afbea01179d232846e7fe718c9e3299
adsonluis1 <dev.adsonluis@gmail.com>
2024-05-14 11:48:16 -0300

    styled twuColuns

---
+++ react/pt_7/src/components/twuColuns/TwuColuns.styled.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+
+export const TwuColunsStyled = styled.section`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 8px;
+    gap: 10px;
+`
