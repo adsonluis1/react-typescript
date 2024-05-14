a63ed9f4662e03e9ed15affa0a6e76c0d257438f
adsonluis1 <dev.adsonluis@gmail.com>
2024-05-14 11:45:57 -0300

    Styled de DivNews

---
+++ react/pt_7/src/components/news/DivNews.styled.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export  const DivNewsStyled = styled.div`
+    display: flex;
+    flex-direction: column;
+    background-color: grey;
+    width: 50%;
+    max-width: 300px;
+    padding: 8px;
+    gap: 10px;
+    & p {
+        font-weight: bolder;
+    }
+`
+
+
