b6a0a564b1c54418bda812cadc43c232bcdecb26
adsonluis1 <dev.adsonluis@gmail.com>
2024-08-14 11:52:40 -0300

    usando zod e react hook form juntos

--- react/reactHookForm/src/App.jsx
+++ react/reactHookForm/src/App.jsx
@@ -1,8 +1,21 @@
 import {useForm} from "react-hook-form"
-import "./global.css" 
+import {z} from 'zod'
+import {zodResolver} from "@hookform/resolvers/zod"
+import "./global.css"
+
 function App() {
-  const {register,handleSubmit,formState:{errors},setError} = useForm({
-    mode:"onChange"
+  const schema = z.object({
+    usuario: z.string().min(3,"No minimo 3 caracteres"),
+    senha: z.string().min(6, "No minimo 6 caracteres"),
+    confirmSenha: z.string().min(6, "No minimo 6 caracteres")
+  }).refine((fields)=> fields.senha === fields.confirmSenha,{
+    path:["confirmSenha"],
+    message:"As senhas precisam ser iguais"
+  })
+
+  const {register,handleSubmit,formState:{errors}} = useForm({
+    mode:"onBlur",
+    resolver: zodResolver(schema)
   })
 
   const handleSubmitData = (data)=> {
@@ -15,13 +28,17 @@ function App() {
        
        <h1>Login</h1>
         <form onSubmit={handleSubmit(handleSubmitData)}>
-          <input autoFocus={true} {...register('usuario', {required:"insira o usuario", pattern:{value:/^a/i,message:"o nome tem que começar com a letra 'a'"}})} type="text" placeholder="Usuario"/>
+          <input autoFocus={true} {...register('usuario')} type="text" placeholder="Usuario"/>
           {errors?.usuario &&
             <p className="error">{errors.usuario?.message}</p>
           }
-          <input {...register('senha',{required:true, minLength:{value:6, message:"A senha precisa ter no minimo 6 caracteres"}})} type="password" placeholder="Senha"/>
+          <input {...register('senha')} type="password" placeholder="Senha"/>
           {errors?.senha &&
             <p className="error">{errors.senha?.message}</p>
+          }
+          <input {...register('confirmSenha')} type="password" placeholder="Confirme a senha"/>
+          {errors?.confirmSenha &&
+            <p className="error">{errors.confirmSenha?.message}</p>
           }
           <input type="submit" value="Enviar" />
         </form>
