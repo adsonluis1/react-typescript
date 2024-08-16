8cb9dad1c38508473e2f7dd2c350db1eb9058083
adsonluis1 <dev.adsonluis@gmail.com>
2024-08-16 16:09:50 -0300

    compents de estilos da pagina home

---
+++ react/validacaoDeCpf/src/pages/home/Home.styled.jsx
@@ -0,0 +1,20 @@
+import {styled} from "styled-components"
+
+export const Main = styled.main`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100vw;
+    height: 100vh;
+`
+
+export const Secition = styled.section`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: aqua;
+    height: 80%;
+    width: 70%;
+    border-radius: 10px;
+    border: 1px solid rgba(0, 0, 0, 0.333);
+`
